12b553a093c7ff4652af43053df1a0fc6bcf6cc3
LyuChaCha <green09good@gmail.com>
2025-08-21 02:28:14 +0800

    test4.2

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from "vuepress";
-
+import { searchPlugin } from "@vuepress/plugin-search";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -10,6 +10,14 @@ export default defineUserConfig({
   description: "title",
 
   theme,
+
+  plugins: [
+    searchPlugin({
+      // 排除首页
+      isSearchable: (page) => page.path !== '/',
+    }),
+  ],
+
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
